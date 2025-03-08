7be8b13fae080a4c05c3dd206dd97c5621b13d2a
hermstefanny <aida.estefania.sosa@gmail.com>
2025-03-08 10:45:36 +0100

    Refactor game logic for consistency and clarity

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 "use strict";
-/*console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "Correct Number!";
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 20;
-*/
 
 //Creating the number
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
@@ -13,9 +8,14 @@ let score = 20;
 let highScore = 0;
 
 //Function penalizing the wrong Guess
-function wrongGuess() {
-  score--;
-  document.querySelector(".score").textContent = score;
+function wrongGuess(messageString) {
+  if (score > 1) {
+    displayMessage(messageString);
+    score--;
+    document.querySelector(".score").textContent = score;
+  } else {
+    displayMessage("Sorry! You lose!");
+  }
 }
 
 //This is the event listener for the Check button
@@ -28,14 +28,14 @@ document.querySelector(".check").addEventListener("click", function () {
   //This is the action to take when the guess does not exist.
   //As we want something to happen, we reverse the value with the ! operator
   if (!guess) {
-    document.querySelector(".message").textContent = "No number!";
+    displayMessage("No number!");
   }
 
   //Second Scenario: Guess is correct
   else if (guess === secretNumber) {
     //assigning the number to the corresponding HTMl element
     document.querySelector(".number").textContent = secretNumber;
-    document.querySelector(".message").textContent = "Great guess! You win!";
+    displayMessage("Great guess! You win!");
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     if (score > highScore) {
@@ -45,23 +45,10 @@ document.querySelector(".check").addEventListener("click", function () {
   }
 
   //Third Scenario: Guess is high
-  else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Guess is too high";
-      wrongGuess();
-    } else {
-      document.querySelector(".message").textContent = "Sorry! you lose!";
-    }
-  }
-
-  //Fourth Scenario: Guess is low
-  else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Guess is too low";
-      wrongGuess();
-    } else {
-      document.querySelector(".message").textContent = "Sorry! you lose!";
-    }
+  else if (guess != secretNumber) {
+    const messageShown =
+      guess > secretNumber ? "Guess is too high" : "Guess is too low";
+    wrongGuess(messageShown);
   }
 });
 
@@ -75,5 +62,8 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
   document.querySelector(".score").textContent = score;
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
 });
+function displayMessage(message) {
+  return (document.querySelector(".message").textContent = message);
+}
